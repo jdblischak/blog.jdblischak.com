2889d327c504c6d02153361bc344fd15f8e594f1
John Blischak <jdblischak@gmail.com>
2019-05-21 14:09:36 -0400

    Add build script for blog.

---
+++ build.R
@@ -0,0 +1,29 @@
+#!/usr/bin/env Rscript
+
+# Only build outdated R Markdown files. Always view in RStudio.
+
+library(fs)
+library(rmarkdown)
+library(rstudioapi)
+
+stopifnot(file_exists("blog.jdblischak.com.Rproj"))
+
+rmd <- dir_ls(path = "_posts/", recurse = TRUE, glob = "*/*Rmd")
+html <- vapply(rmd, path_ext_set, character(1), ext = "html")
+outdated <- !file_exists(html) |
+            file_info(rmd)$modification_time > file_info(html)$modification_time
+
+for (r in rmd[outdated]) {
+  cat("Rendering", r, "\n")
+  render(r, quiet = TRUE)
+}
+
+if (sum(outdated) > 0) {
+  render_site(input = "index.Rmd", encoding = "UTF-8")
+}
+
+if (isAvailable()) {
+  tmp <- fs::file_temp()
+  dir_copy("public/", tmp)
+  viewer(path(tmp, "index.html"))
+}
