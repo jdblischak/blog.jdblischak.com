312a96003271c9f31fb18d69005a6e905ccfc262
John Blischak <jdblischak@gmail.com>
2020-02-11 15:25:16 -0500

    Add CI-specific build script.

---
+++ build-ci.R
@@ -0,0 +1,54 @@
+#!/usr/bin/env Rscript
+
+# Only build R Markdown files that have been committed more recently than their
+# corresponding HTML files.
+
+library(git2r)
+library(fs)
+library(rmarkdown)
+library(rstudioapi)
+
+stopifnot(file_exists("blog.jdblischak.com.Rproj"))
+is_ci_build <- identical(Sys.getenv("CIRCLECI"), "true")
+if (!is_ci_build) warning("This isn't a CI build", immediate. = TRUE)
+
+rmd <- dir_ls(path = "_posts/", recurse = TRUE, glob = "*/*Rmd")
+# https://github.com/r-lib/fs/pull/208
+html <- vapply(rmd, path_ext_set, character(1), ext = "html")
+r <- repository()
+
+outdated <- logical(length = length(rmd))
+for (i in seq_along(rmd)) {
+  rmd_commits <- commits(r, path = rmd[i])
+  if (length(rmd_commits) == 0) {
+    message("Skipped because not tracked: ", rmd[i])
+    next
+  }
+
+  if (!file_exists(html[i])) {
+    outdated[i] <- TRUE
+    next
+  }
+
+  html_commits <- commits(r, path = html[i])
+  if (length(html_commits) == 0) {
+    outdated[i] <- TRUE
+    next
+  }
+
+  rmd_last_commit_time <- as.POSIXct(rmd_commits[[1]]$author$when)
+  html_last_commit_time <- as.POSIXct(html_commits[[1]]$author$when)
+  if (rmd_last_commit_time > html_last_commit_time) {
+    outdated[i] <- TRUE
+  }
+}
+
+for (r in rmd[outdated]) {
+  cat("Rendering", r, "\n")
+  render(r, quiet = TRUE)
+}
+
+if (sum(outdated) > 0) {
+  cat("Rebuilding site\n")
+  render_site(input = "index.Rmd", encoding = "UTF-8")
+}
